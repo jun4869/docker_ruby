645c7d53f90946849f439eebc418da02317910ec
jun <jun2194k@gmail.com>
2021-05-30 18:03:56 +0900

    add

--- nullllll_app/app/controllers/recipes_controller.rb
+++ nullllll_app/app/controllers/recipes_controller.rb
@@ -18,6 +18,18 @@ class RecipesController < ApplicationController
     redirect_to recipe_path(@recipe)
   end
 
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe), notice: "レシピを更新しました。"
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect_to user_path(recipe.user), notice: "レシピを削除しました。"
+  end
+
   def edit
     @recipe = Recipe.find(params[:id])
   end
